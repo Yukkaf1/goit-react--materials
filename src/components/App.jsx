26ba064bfbbdac4ea4cddda3a5dd0710c6945a89
Trunova Yulia <103053349+Yukkaf1@users.noreply.github.com>
2023-04-07 21:13:55 +0200

    add materiasl

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,16 +11,24 @@ export class App extends Component {
     error: false,
   };
 
-  async addMaterial(values) {
-    const material = await API.addMaterial(values);
-    this.setState(state => ({ materials: [...state.materials, material] }));
-    console.log(material);
-  }
+  addMaterial = async values => {
+    try {
+      const material = await API.addMaterial(values);
+      this.setState(state => ({
+        materials: [...state.materials, material],
+      }));
+    } catch (error) {
+      this.setState({ error: true, isLoading: false });
+      console.log(error);
+    }
+  };
 
   render() {
+    const { materials, isLoading, error } = this.state;
     return (
       <Layout>
         <GlobalStyle />
+        {this.state.isLoading && <div>LOADING</div>}
         <MaterialEditorForm onSubmit={this.addMaterial}></MaterialEditorForm>
       </Layout>
     );
