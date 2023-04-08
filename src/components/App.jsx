767dfa013161f027588407a5a305bf932d2a0928
Trunova Yulia <103053349+Yukkaf1@users.noreply.github.com>
2023-04-08 18:18:47 +0200

    delete / update material

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { GlobalStyle } from './GlobalStyle';
 import { MaterialEditorForm } from './MaterialEditorForm/MaterialEditorForm';
 import { Layout } from './Layout';
 import * as API from 'services/api';
+import { MaterialList } from './MaterialList/MaterialList';
 
 export class App extends Component {
   state = {
@@ -10,6 +11,17 @@ export class App extends Component {
     isLoading: false,
     error: false,
   };
+
+  async componentDidMount() {
+    try {
+      this.setState({ isLoading: true });
+      const materials = await API.getMaterials();
+      this.setState({ materials, isLoading: false });
+    } catch (error) {
+      this.setState({ error: true, isLoading: false });
+      console.log(error);
+    }
+  }
 
   addMaterial = async values => {
     try {
@@ -23,13 +35,54 @@ export class App extends Component {
     }
   };
 
+  deleteMaterial = async id => {
+    try {
+      await API.deleteMaterial(id);
+      this.setState(state => ({
+        materials: state.materials.filter(material => material.id !== id),
+      }));
+    } catch (error) {
+      this.setState({ error: true });
+      console.log(error);
+    }
+  };
+
+  updateMaterial = async fields => {
+    try {
+      const updatedMaterial = await API.updateMaterial(fields);
+      this.setState(state => ({
+        materials: state.materials.map(material =>
+          material.id === fields.id ? updatedMaterial : material
+        ),
+      }));
+    } catch (error) {
+      this.setState({ error: true });
+      console.log(error);
+    }
+  };
+
   render() {
     const { materials, isLoading, error } = this.state;
     return (
       <Layout>
         <GlobalStyle />
+        {error && (
+          <p>
+            Ой! Что-то пошло не так. Перезагрузите страницу и попробуйте еще
+            раз.
+          </p>
+        )}
         {this.state.isLoading && <div>LOADING</div>}
         <MaterialEditorForm onSubmit={this.addMaterial}></MaterialEditorForm>
+        {isLoading ? (
+          'Загружаем материалы'
+        ) : (
+          <MaterialList
+            items={materials}
+            onDelete={this.deleteMaterial}
+            onUpdate={this.updateMaterial}
+          />
+        )}
       </Layout>
     );
   }
